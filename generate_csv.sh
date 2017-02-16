8d12e59386ee3ff9bf2e0386c1d84c7d2db0863f
Lukas Martinelli <me@lukasmartinelli.ch>
2017-02-16 15:35:53 +0100

    Add population datasets

--- generate_csv.sh
+++ generate_csv.sh
@@ -5,19 +5,20 @@ set -o nounset
 
 function clear_files() {
   rm -rf energy
+  rm -rf work_income
+  rm -rf crime
 }
 
 function gen_csv() {
   local file_index=./files.csv
-  sed 1d "$file_index" | while IFS=, read category filename datum_column title source_url download_url; do
+  sed 1d "$file_index" | while IFS=, read category filename datum_column source_url download_url title; do
     mkdir -p "$category"
     local px_filename="$category/$filename.px"
     local csv_filename="$category/$filename"
     wget --quiet -O "$px_filename" "$download_url"
     iconv -t "UTF-8" -f "ISO-8859-1" "$px_filename" -o "$px_filename.utf8"
-    #rm "$px_filename"
     mv "$px_filename.utf8" "$px_filename"
-    px-to-csv -i "$px_filename" -o "$csv_filename" -c "$datum_column"
+    timeout 120 px-to-csv -i "$px_filename" -o "$csv_filename" -c "$datum_column"
     rm "$px_filename"
     echo "$csv_filename"
   done
